033c4e5940deb786dd539a1bbcad8f66bd04ece9
Amaury Peillon <apeillon@digital-village.fr>
2023-04-12 19:21:17 +0200

    Update mobile-menu.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/components/mobile-menu.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/components/mobile-menu.blade.php
@@ -8,7 +8,7 @@
       </div>
       <div class="col-8">
         <a href="<?php echo home_url(); ?>" aria-label="Site Le Pitch">
-          <img src="@asset('images/lepitch.svg')" alt="Logo Le Pitch" class="d-block logo mx-auto" style="max-width: 200px">
+          <img src="@asset('images/lepitch.svg')" width="200px" height="48px" alt="Logo Le Pitch" class="d-block logo mx-auto" style="max-width: 200px">
         </a>
       </div>
       <div class="col-2">
