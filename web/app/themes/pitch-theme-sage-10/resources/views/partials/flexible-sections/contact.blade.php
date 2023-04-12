d249bac0518cb3ac3d8fd25def496b12e0a50ad4
Amaury Peillon <apeillon@digital-village.fr>
2023-04-12 19:02:06 +0200

    Update contact.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/contact.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/contact.blade.php
@@ -20,6 +20,7 @@
                 <span class="d-block mb-4">15 Rue Vandrezanne - 75013 Paris</span>
                 <div class="ratio ratio-16x9">
                   <iframe
+                    title="Map Paris"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2626.58758381038!2d2.3530185515265014!3d48.82792987918287!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6718fb114b69b%3A0x7121054a2d09dc67!2s15%20Rue%20Vandrezanne%2C%2075013%20Paris!5e0!3m2!1sfr!2sfr!4v1588343834517!5m2!1sfr!2sfr"
                     width="600" height="450" frameborder="0" style="border:0;"
                     allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
@@ -30,6 +31,7 @@
                 <span class="d-block mb-4">7 rue d'Autun - 06000 Nice</span>
                 <div class="ratio ratio-16x9">
                   <iframe
+                    title="Map Nice"
                     src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d46152.036826509626!2d7.281990284283433!3d43.70010937649066!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2bffbaf16cdf9917!2sLe%20Pitch%20Web!5e0!3m2!1sfr!2sfr!4v1588343783282!5m2!1sfr!2sfr"
                     width="600" height="450" frameborder="0" style="border:0;"
                     allowfullscreen=""
