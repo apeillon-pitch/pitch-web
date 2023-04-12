f9307524a21987e38d7350d74f39f854ac93b0ec
Amaury Peillon <apeillon@digital-village.fr>
2023-04-12 17:56:57 +0200

    mobile menu

--- web/app/themes/pitch-theme-sage-10/resources/views/components/mobile-menu.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/components/mobile-menu.blade.php
@@ -1,9 +1,9 @@
-<nav id="c-menu--slide-right" class="c-menu c-menu--slide-right">
+<div id="c-menu--slide-right" class="c-menu c-menu--slide-right">
   <div class="container-fluid h-100">
     <div class="row align-items-center">
       <div class="col-2">
-        <button class="c-menu__close">
-          <i class="fas fa-times"></i>
+        <button title="Fermer le menu" class="c-menu__close">
+          <i class="fa-light fa-xmark"></i>
         </button>
       </div>
       <div class="col-8">
@@ -19,11 +19,11 @@
       <div class="col-12 mt-5">
         <nav id="navbar-mobile" class="nav-primary navbar">
           @if (has_nav_menu('primary_navigation'))
-            {!! wp_nav_menu($primarymenu) !!}
+            {!! wp_nav_menu($mainMenu) !!}
           @endif
         </nav>
       </div>
     </div>
   </div>
-</nav>
+</div>
 <div id="c-mask" class="c-mask"></div>
