9ccb9b0cae1e783a779c04a747b8a3b8196f2d26
Amaury Peillon <apeillon@digital-village.fr>
2023-04-13 10:22:28 +0200

    cta col image text

--- web/app/themes/pitch-theme-sage-10/index.php
+++ web/app/themes/pitch-theme-sage-10/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet" media="print" onload="this.onload=null;this.media='all'">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet" media="print" onload="this.onload=null;this.media='all'">
     <script src="https://kit.fontawesome.com/0b51096d56.js" defer crossorigin="anonymous"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119894519-1"></script>
--- web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/image_text_2col.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/image_text_2col.blade.php
@@ -1,20 +1,30 @@
 <div id="section-{{ $row }}"
      class="section-image-text-2col {!! $section['padding_top'] !!} {!! $section['padding_bottom'] !!} {!! $section['margin_top'] !!} {!! $section['margin_bottom'] !!}">
-    <div class="container">
-        <div class="row align-items-center justify-content-center">
-            <div class="d-none d-lg-block col-lg-4">
-                @php $image = $section['image']['sizes'][ 'medium' ] @endphp
-                <img src="{{ $image }}" class="img-fluid" alt="Le Pitch Web">
-            </div>
-            <div class="col-11 col-lg-8">
-                @if ( $section['section_title'])
-                    @include('partials.flexible-sections.section_title')
-                @endif
-                @if ( $section['text'])
-                    {!! $section['text'] !!}
-                @endif
-            </div>
+  <div class="container">
+    @if ( $section['section_title'])
+      @include('partials.flexible-sections.section_title')
+    @endif
+    <div class="row align-items-center justify-content-center">
+      <div class="d-none d-lg-block col-lg-3 pe-5">
+        {!! wp_get_attachment_image( $section['image']['id'], 'medium','', array( "class" => "img-fluid")) !!}
+      </div>
+      <div class="col-11 col-lg-8">
+        @if ( $section['text'])
+          {!! $section['text'] !!}
+        @endif
+        <div class="d-flex flex-row mt-4">
+          <a class="button button-arrow me-4" href="{{ home_url() }}/#section-7">
+            <span class="button-shift"><span class="button-underline">nos clients</span></span>
+          </a>
+          <a class="button button-arrow me-4" href="{{ home_url() }}/#section-6">
+            <span class="button-shift"><span class="button-underline">nos expertises</span></span>
+          </a>
+          <a class="button button-arrow" href="{{ home_url() }}/contact/">
+            <span class="button-shift"><span class="button-underline">contact</span></span>
+          </a>
         </div>
+      </div>
     </div>
+  </div>
 </div>
 
