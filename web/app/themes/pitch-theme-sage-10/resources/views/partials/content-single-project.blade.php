eaab9f097f06e6d05b9fdd9368b5f31cf2375153
Amaury Peillon <apeillon@digital-village.fr>
2023-06-22 14:34:39 +0200

    Update content-single-project.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/content-single-project.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/content-single-project.blade.php
@@ -9,6 +9,7 @@
       <div class="col-12 col-lg-6 p-4 text-center text-lg-left">
         <h1 class="entry-title">{!! get_the_title() !!}</h1>
         <ul class="post-categories">
+          {{ var_dump($portfolio_data['category']) }}
         @foreach($portfolio_data['category'] as $item)
             <li><a href="#" rel="category tag">{!! $item->name !!}</a></li>
           @endforeach
