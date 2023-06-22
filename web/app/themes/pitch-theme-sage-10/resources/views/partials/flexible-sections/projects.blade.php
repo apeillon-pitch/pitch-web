cadb71379b721b0010f561f4de6b5d661480456d
Amaury Peillon <apeillon@digital-village.fr>
2023-06-22 13:12:58 +0200

    Create projects.blade.php

---
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/projects.blade.php
@@ -0,0 +1,14 @@
+<div id="section-{{ $row }}"
+     class="section-key-figures {!! $section['padding_top'] !!} {!! $section['padding_bottom'] !!} {!! $section['margin_top'] !!} {!! $section['margin_bottom'] !!}">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-12 text-center">
+        <h1 class="section-title">Portfolio</h1>
+        <div class="row">
+          @php echo do_shortode('[ess_grid alias="portfolio"][/ess_grid]') @endphp
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
