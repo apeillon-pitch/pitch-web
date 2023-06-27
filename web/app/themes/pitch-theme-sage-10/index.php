9162e26b9f3aa841c47af2bf6b4cae53fcb0cd9a
Amaury Peillon <apeillon@digital-village.fr>
2023-06-27 11:37:46 +0200

    Update index.php

--- web/app/themes/pitch-theme-sage-10/index.php
+++ web/app/themes/pitch-theme-sage-10/index.php
@@ -8,14 +8,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet" media="print" onload="this.onload=null;this.media='all'">
     <script src="https://kit.fontawesome.com/0b51096d56.js" defer crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@srexi/purecounterjs/dist/purecounter_vanilla.js"></script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119894519-1"></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q0RE2W7WLL"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
-      gtag('config', 'UA-119894519-1');
+      gtag('config', 'G-Q0RE2W7WLL');
     </script>
     <?php wp_head(); ?>
   </head>
