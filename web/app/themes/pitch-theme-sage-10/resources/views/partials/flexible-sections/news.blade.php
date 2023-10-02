4e7e60083bea2923eac300484ccc393388f16571
apeillon-pitch <apeillon@lepitch.fr>
2023-10-02 17:11:20 +0200

    update

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/news.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/news.blade.php
@@ -1,17 +1,19 @@
 <div id="section-{{ $row }}"
      class="section-news section-bg-color {!! $section['padding_top'] !!} {!! $section['padding_bottom'] !!} {!! $section['margin_top'] !!} {!! $section['margin_bottom'] !!}">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-12">
-                @if ( $section['section_title'])
-                    @include('partials.flexible-sections.section_title')
-                @endif
-                  @if ( $section['section_subtitle'])
-                    @include('partials.flexible-sections.section_subtitle')
-                  @endif
-                @include('partials.flexible-sections.news_list')
-            </div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-12">
+        <div class="wp-title">
+          @if ( $section['section_title'])
+            @include('partials.flexible-sections.section_title')
+          @endif
+          @if ( $section['section_subtitle'])
+            @include('partials.flexible-sections.section_subtitle')
+          @endif
         </div>
+        @include('partials.flexible-sections.news_list')
+      </div>
     </div>
+  </div>
 </div>
 
