cdfd26ce9f036d356755c4fa08aa4c24f8ea24f7
apeillon-pitch <apeillon@lepitch.fr>
2023-10-03 10:31:43 +0200

    update

---
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/text_2col.blade.php
@@ -0,0 +1,27 @@
+<div id="section-{{ $row }}"
+     class="section-text-2col animated-section {!! $section['padding_top'] !!} {!! $section['padding_bottom'] !!} {!! $section['margin_top'] !!} {!! $section['margin_bottom'] !!}">
+  <div class="container" data-aos="fade-up">
+    <div class="row justify-content-center">
+      <div class="col-4">
+        @if ( $section['section_title'])
+          @include('partials.flexible-sections.section_title')
+        @endif
+      </div>
+      <div class="col-8 ps-5">
+        @if($section['item_repeater'])
+          @foreach($section['item_repeater'] as $item)
+            <div class="row @php echo $loop->iteration % 2 == 0 ? 'justify-content-end' : '' @endphp">
+              <div class="col-6">
+                @if ( $item['section_subtitle'])
+                  <span class="title mb-2">{!! $item['section_subtitle'] !!}</span>
+                @endif
+                {!! $item['text'] !!}
+              </div>
+            </div>
+          @endforeach
+        @endif
+      </div>
+    </div>
+  </div>
+</div>
+
