cea96c79e7fc4a9220b11c21ff3f92c12b96d6e1
apeillon-pitch <apeillon@lepitch.fr>
2023-10-04 17:49:15 +0200

    Update banner_cta_style_one.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/banner_cta_style_one.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/banner_cta_style_one.blade.php
@@ -1,13 +1,13 @@
 <div id="section-{{ $row }}"
      class="section banner-cta mt-none mb">
   <div class="container text-start">
-    <div class="row justify-content-between align-items-center">
-      <div class="col-4 pe-5">
+    <div class="row justify-content-center justify-content-md-between align-items-center">
+      <div class="d-none d-md-block col-md-4 pe-5">
         @if($section['image'])
           {!! wp_get_attachment_image( $section['image']['id'], 'full','', array( "class" => "img-fluid")) !!}
         @endif
       </div>
-      <div class="col-8 ps-5">
+      <div class="col-11 col-md-8 ps-5">
         @if ( $section['section_title'])
           @include('partials.flexible-sections.section_title')
         @endif
