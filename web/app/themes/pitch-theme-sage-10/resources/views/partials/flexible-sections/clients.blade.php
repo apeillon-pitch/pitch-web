6a253c46897bc554295bc50c801d1b77a452b990
apeillon-pitch <apeillon@lepitch.fr>
2023-10-20 15:19:20 +0200

    Update clients.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/clients.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/clients.blade.php
@@ -2,7 +2,7 @@
      class="section-clients section-bg-color {!! $section['padding_top'] !!} {!! $section['padding_bottom'] !!} {!! $section['margin_top'] !!} {!! $section['margin_bottom'] !!}">
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-12 col-lg-12">
+      <div class="col-11 col-lg-12">
         @if ( $section['section_title'])
           @include('partials.flexible-sections.section_title')
         @endif
