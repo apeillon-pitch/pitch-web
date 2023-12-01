cb096ccdac93be00573642e68dbc1fef28c9e9d0
apeillon-pitch <apeillon@lepitch.fr>
2023-12-01 15:10:28 +0100

    Update key_figures.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/key_figures.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/key_figures.blade.php
@@ -12,11 +12,11 @@
             <div class="col-6 col-sm-3 mb-3 ps-4 pe-4 mb-lg-0 text-start block">
               <div class="d-block w-100">
                 <div class="d-inline-block mx-auto position-relative wp">
-              <span class="d-none d-md-block number purecounter text-center font-weight-bold"
+              <span class="number purecounter text-center font-weight-bold"
                     data-purecounter-start="0" data-purecounter-duration="1"
                     data-purecounter-delay="15" data-purecounter-once="true"
-                    data-purecounter-end="{{ $block['data'] }}">0</span>
-                  <span class="d-block d-md-none numbzer text-center font-weight-bold">{{ $block['data'] }}</span>
+                    data-purecounter-end="{{ $block['data'] }}">{{ $block['data'] }}</span>
+                  <span class="d-block numbzer text-center font-weight-bold">{{ $block['data'] }}</span>
                   @if ($block['suffix'])
                     <span class="suffix">{!! $block['suffix'] !!}</span>
                   @endif
