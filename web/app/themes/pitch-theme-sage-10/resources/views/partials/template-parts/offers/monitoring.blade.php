07a3a20fa3b9e4b15730fdad6075f0d259e65bd0
apeillon-pitch <apeillon@lepitch.fr>
2024-05-06 14:52:59 +0200

    Update monitoring.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/template-parts/offers/monitoring.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/template-parts/offers/monitoring.blade.php
@@ -4,7 +4,7 @@
       @if($offer != "one" AND $offer != "two")
         <img src="@asset('images/checkmark-circle-green.svg')" class="me-2" alt="non inclus">
       @endif
-      @if($offer === "one" OR $offer === "two"))
+      @if($offer === "one" OR $offer === "two")
         <img src="@asset('images/checkmark-circle-grey.svg')" class="me-2" alt="inclus">
       @endif
       <strong>Monitoring</strong>
