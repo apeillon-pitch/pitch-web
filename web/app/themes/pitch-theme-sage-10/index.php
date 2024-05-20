f594c1db5192349cd4b46786bba573d93dcb892e
apeillon-pitch <apeillon@lepitch.fr>
2024-05-20 17:05:22 +0200

    Update index.php

--- web/app/themes/pitch-theme-sage-10/index.php
+++ web/app/themes/pitch-theme-sage-10/index.php
@@ -5,11 +5,12 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link rel="preconnect" href="https://www.googletagmanager.com">
     <?php if (is_front_page()) { ?>
       <link rel="preload" fetchpriority="high" as="image" href="<?php echo home_url(); ?>/app/uploads/2023/06/Teaser-1.png.webp" type="image/webp">
     <?php } ?>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet" media="print" onload="this.onload=null;this.media='all'">
-    <script src="https://cdn.jsdelivr.net/npm/@srexi/purecounterjs/dist/purecounter_vanilla.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@srexi/purecounterjs/dist/purecounter_vanilla.js" defer></script>
     <!-- Google tag (gtag.js) -->
   <!--  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-857100355">
     </script>
@@ -21,20 +22,26 @@
       gtag('config', 'AW-857100355');
     </script>-->
     <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-PDCV8LKM');</script>
+    <script>
+      (function(w,d,s,l,i){
+        w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+        var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PDCV8LKM');
+    </script>
     <!-- End Google Tag Manager -->
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDCV8LKM"
-                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
     <?php wp_head(); ?>
   </head>
 
   <body <?php body_class(); ?>>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDCV8LKM"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+  </noscript>
+  <!-- End Google Tag Manager (noscript) -->
     <?php wp_body_open(); ?>
     <?php do_action('get_header'); ?>
 
