9b838c74b83dbcf8f37250f1670aaafae13895e9
apeillon-pitch <apeillon@lepitch.fr>
2024-05-23 10:30:53 +0200

    Update clients.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/clients.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/clients.blade.php
@@ -27,7 +27,7 @@
           <div class="row justify-content-center">
             @foreach($section['logos_repeater'] as $index => $logo)
               @if($index < 60)
-                <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xxl-2 mb-4">
+                <div class="col-6 col-md-4 col-lg-3 col-xxl-2 mb-4">
                   <div class="logo-container">
                     {!! wp_get_attachment_image(  $logo['logo']['id'], 'full', '', array( "class" => "img-fluid") ) !!}
                   </div>
