77827c860de2c70e9645951c6b9aa317b51b0eef
apeillon-pitch <apeillon@lepitch.fr>
2024-09-04 08:29:14 +0200

    update

--- web/app/themes/pitch-theme-sage-10/resources/scripts/app.js
+++ web/app/themes/pitch-theme-sage-10/resources/scripts/app.js
@@ -177,10 +177,8 @@ const main = async (err) => {
       dots: false,
       infinite: true,
       speed: 400,
-      fade: true,
-      cssEase: 'linear',
       autoplay: true,
-      autoplaySpeed: 3000,
+      autoplaySpeed: 4000,
       pauseOnHover: false,
     });
   }
