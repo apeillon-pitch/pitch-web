ad7f5e29ef5b4ffac2743dee845d81f85e36cfeb
apeillon-pitch <apeillon@lepitch.fr>
2024-09-06 09:42:17 +0200

    Update 404.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/404.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/404.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
       <div class="d-flex flex-column align-items-center">
         @if (! have_posts())
-          <h1 class="section-title mb-5">Cette page d'existe plus :(</h1>
+          <h1 class="section-title mb-5">Cette page n'existe plus !</h1>
           <div class="d-flex flex-row justify-content-center mb-5 pb-5">
             <a href="{{ home_url() }}" aria-label="Retour à l'accueil" class="btn btn-primary">Retour à l'accueil</a>
             <a href="{{ home_url() }}" aria-label="Retour à l'accueil" class="btn btn-primary ms-3">Nous contacter</a>
