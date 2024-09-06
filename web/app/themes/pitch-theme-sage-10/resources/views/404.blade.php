a8a0a45ab531e346f5cd9aa33ac83ac6e1f52e5a
apeillon-pitch <apeillon@lepitch.fr>
2024-09-06 09:36:16 +0200

    Update 404.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/404.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/404.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+  <div class="section pt pb">
+    <div class="container">
+      @if (! have_posts())
+        <x-alert type="warning">
+          {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
+        </x-alert>
 
-  @if (! have_posts())
-    <x-alert type="warning">
-      {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
-    </x-alert>
-
-    {!! get_search_form(false) !!}
-  @endif
+        {!! get_search_form(false) !!}
+      @endif
+    </div>
+  </div>
 @endsection
