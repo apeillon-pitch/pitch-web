180376273f23a7603d7c28437164e04eb44468d9
apeillon-pitch <apeillon@lepitch.fr>
2024-09-06 09:40:11 +0200

    Update 404.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/404.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/404.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-    <div class="d-flex flex-column">
-      @if (! have_posts())
-        <h1 class="section-title mb-5">Cette page d'existe plus :(</h1>
-        <a href="{{ home_url() }}" aria-label="Retour à l'accueil" class="btn btn-primary">Retour à l'accueil</a>
-      @endif
+  <div class="section-news pt pb mt mb">
+    <div class="container">
+      <div class="d-flex flex-column">
+        @if (! have_posts())
+          <h1 class="section-title mb-5">Cette page d'existe plus :(</h1>
+          <a href="{{ home_url() }}" aria-label="Retour à l'accueil" class="btn btn-primary">Retour à l'accueil</a>
+        @endif
+      </div>
     </div>
   </div>
 @endsection
