650a04aece0f41193078295c571be01e67981921
apeillon-pitch <apeillon@lepitch.fr>
2024-10-21 15:26:41 +0200

    update

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/title_text_style_one.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/title_text_style_one.blade.php
@@ -2,7 +2,7 @@
      class="section section-title-text pt {!! $section['padding_bottom'] !!} {!! $section['margin_top'] !!} {!! $section['margin_bottom'] !!}">
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-11 col-lg-8">
+      <div class="col-11 col-lg-10 col-xx-9">
         @if ( $section['section_title'])
           @include('partials.flexible-sections.section_title')
         @endif
