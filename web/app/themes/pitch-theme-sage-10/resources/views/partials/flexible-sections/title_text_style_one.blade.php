9109bb23b94d5faf80a23cbe49bcdf72d59cc502
apeillon-pitch <apeillon@lepitch.fr>
2024-10-21 15:24:41 +0200

    update

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/title_text_style_one.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/title_text_style_one.blade.php
@@ -1,13 +1,15 @@
 <div id="section-{{ $row }}"
-     class="section section-cta-style-two pt {!! $section['padding_bottom'] !!} {!! $section['margin_top'] !!} {!! $section['margin_bottom'] !!}">
+     class="section section-title-text pt {!! $section['padding_bottom'] !!} {!! $section['margin_top'] !!} {!! $section['margin_bottom'] !!}">
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-11 col-lg-8 col-xxl-7">
+      <div class="col-11 col-lg-8">
         @if ( $section['section_title'])
           @include('partials.flexible-sections.section_title')
         @endif
         @if($section['text'])
-          {!! $section['text'] !!}
+          <div class="wp-text">
+            {!! $section['text'] !!}
+          </div>
         @endif
       </div>
     </div>
