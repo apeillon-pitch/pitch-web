580f3590d00becae90e952ccc08b1b3554e1dd4a
apeillon-pitch <apeillon@lepitch.fr>
2024-10-24 11:01:32 +0200

    update

--- web/app/themes/pitch-theme-sage-10/index.php
+++ web/app/themes/pitch-theme-sage-10/index.php
@@ -6,9 +6,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
- <!-- <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
-        rel="stylesheet" media="print" onload="this.onload=null;this.media='all'">-->
+  <link
+    href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+    rel="stylesheet">
+  <!-- <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
+         rel="stylesheet" media="print" onload="this.onload=null;this.media='all'">-->
   <!-- Google Tag Manager -->
   <script>(function (w, d, s, l, i) {
       w[l] = w[l] || [];
@@ -30,7 +32,11 @@
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q0RE2W7WLL"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+
     gtag('js', new Date());
 
     gtag('config', 'G-Q0RE2W7WLL');
@@ -55,21 +61,25 @@ $splash_colors = ['#231f20', '#fff'];
 $random_splash_color = $splash_colors[array_rand($splash_colors)];
 ?>
 
-<div id="splash-screen" style="background-color: <?php echo esc_attr($random_splash_color); ?>">
+<div id="splash-screen" class="d-flex flex-column" style="background-color: <?php echo esc_attr($random_splash_color); ?>">
+  <div class="lds-ripple">
+    <div></div>
+    <div></div>
+  </div>
   <p class="d-flex flex-column justify-content-center align-items-center" style="margin-top:90px; gap:50px;">
-    <?php if($random_splash_color == '#fff') { ?>
-    <img src="<?php echo get_template_directory_uri(); ?>/resources/images/lepitch.svg"
-         class="logo" alt="Agence Wordpress Paris et Nice" width="200px" height="auto" style="max-width: 200px">
+    <?php if ($random_splash_color == '#fff') { ?>
+      <img src="<?php echo get_template_directory_uri(); ?>/resources/images/lepitch.svg"
+           class="logo" alt="Agence Wordpress Paris et Nice" width="200px" height="auto" style="max-width: 200px">
     <?php } else { ?>
-    <img src="<?php echo get_template_directory_uri(); ?>/resources/images/pitch-white.svg"
-         class="logo" alt="Agence Wordpress Paris et Nice" width="200px" height="auto" style="max-width: 200px">
+      <img src="<?php echo get_template_directory_uri(); ?>/resources/images/pitch-white.svg"
+           class="logo" alt="Agence Wordpress Paris et Nice" width="200px" height="auto" style="max-width: 200px">
     <?php } ?>
-    <?php if($random_splash_color == '#fff') {
+    <?php if ($random_splash_color == '#fff') {
       $color = '#231f20';
     } else {
       $color = 'white';
-    }?>
-    <strong style="color:<?php echo $color; ?>">Agence WordPress</strong>
+    } ?>
+  <strong style="color:<?php echo $color; ?>">Agence WordPress</strong>
   </p>
 </div>
 <?php wp_body_open(); ?>
