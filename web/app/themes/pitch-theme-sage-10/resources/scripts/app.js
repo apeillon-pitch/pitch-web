2e763fcfdb2122f91774ce44b863dd5cf6730703
apeillon-pitch <apeillon@lepitch.fr>
2024-10-24 14:05:08 +0200

    update

--- web/app/themes/pitch-theme-sage-10/resources/scripts/app.js
+++ web/app/themes/pitch-theme-sage-10/resources/scripts/app.js
@@ -4,7 +4,6 @@ import Dropdown from 'bootstrap/js/dist/dropdown'
 import Modal from 'bootstrap/js/dist/modal'
 import Tab from 'bootstrap/js/dist/tab'
 import './slide-menu';
-import 'select2';
 import './preloader';
 
 /**
