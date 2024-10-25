1b732b3f5c84bb283eb2fd6d0eca9f421bf34504
apeillon-pitch <apeillon@lepitch.fr>
2024-10-25 14:53:48 +0200

    Update setup.php

--- web/app/themes/pitch-theme-sage-10/app/setup.php
+++ web/app/themes/pitch-theme-sage-10/app/setup.php
@@ -137,6 +137,14 @@ add_action('widgets_init', function () {
 });
 
 /**
+ * Image size
+ */
+
+add_action( 'after_setup_theme', 'image_theme_setup', function () {
+    add_image_size( 'logo-slideshow', 115 ); // 115 pixels wide (and unlimited height)
+});
+
+/**
  * Add favicons to <head>.
  *
  */
