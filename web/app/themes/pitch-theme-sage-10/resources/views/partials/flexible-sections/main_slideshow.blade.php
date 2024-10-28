bdfa21bd51464c3327381378e7a79fa12ccbc6b6
apeillon-pitch <apeillon@lepitch.fr>
2024-10-28 14:31:17 +0100

    Update main_slideshow.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/main_slideshow.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/main_slideshow.blade.php
@@ -14,7 +14,7 @@
                            alt="Wordpress" class="me-2">
                     @endif
                     <h1 class="overtitle mb-0">
-                      {!! $slide['title'] !!} dp
+                      {!! $slide['title'] !!} d
                     </h1>
                   </div>
                 @endif
